6fa774b55e9b8486d5b6303aaa00ff9c4b5d2b56
8spw <formyauto75@gmail.com>
2025-12-29 01:56:55 +0400

    feat: code clean up

---
+++ enemy_stats.py
@@ -0,0 +1,6 @@
+enemies = {
+    "soldier": {"hp": 300, "difficulty": 0.8, "runes": 500},
+    "knight": {"hp": 600, "difficulty": 1.0, "runes": 3000},
+    "demigod": {"hp": 1200, "difficulty": 1.4, "runes": 20000},
+    "dragon": {"hp": 2000, "difficulty": 1.8, "runes": 50000},
+}
